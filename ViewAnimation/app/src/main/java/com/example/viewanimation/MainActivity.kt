fe3f6480e0968b599fa5d1f9cf8f5c34a0d5717d
mehdi <powergame_coder2@yahoo.com>
2022-07-22 08:07:43 +0430

    Defined animation in resources

--- ViewAnimation/app/src/main/java/com/example/viewanimation/MainActivity.kt
+++ ViewAnimation/app/src/main/java/com/example/viewanimation/MainActivity.kt
@@ -17,30 +17,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.start.setOnClickListener {button->
-            val myAnimationSet = AnimationSet(true)
-            myAnimationSet.duration = 3000
-            val translate = TranslateAnimation(
-                0f,
-            300f,
-                 0f,
-                 300f
-            )
+            val myResAnimSet = AnimationUtils.loadAnimation(this, R.anim.my_anim)
 
-            val rotate = RotateAnimation(0f, 360f)
-            val alpha = AlphaAnimation(1f, 0f)
-            val scale = ScaleAnimation(
-                1f, 3f,
-            1f, 3f)
-
-
-            myAnimationSet.apply{
-                addAnimation(translate)
-                addAnimation(rotate)
-                addAnimation(alpha)
-                addAnimation(scale)
-            }
-
-            button.startAnimation(myAnimationSet)
+            button.startAnimation(myResAnimSet)
+//            val myAnimationSet = AnimationSet(true)
+//            myAnimationSet.duration = 3000
+//            val translate = TranslateAnimation(
+//                0f,
+//            300f,
+//                 0f,
+//                 300f
+//            )
+//
+//            val rotate = RotateAnimation(0f, 360f)
+//            val alpha = AlphaAnimation(1f, 0f)
+//            val scale = ScaleAnimation(
+//                1f, 3f,
+//            1f, 3f)
+//
+//
+//            myAnimationSet.apply{
+//                addAnimation(translate)
+//                addAnimation(rotate)
+//                addAnimation(alpha)
+//                addAnimation(scale)
+//            }
+//
+//            button.startAnimation(myAnimationSet)
 
         }
 
