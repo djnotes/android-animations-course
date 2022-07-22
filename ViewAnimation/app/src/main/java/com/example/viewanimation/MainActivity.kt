fd22feac39d1b1bc19319d325654863f0c2f5a47
mehdi <powergame_coder2@yahoo.com>
2022-07-22 08:42:23 +0430

    Define animation between animations

--- ViewAnimation/app/src/main/java/com/example/viewanimation/MainActivity.kt
+++ ViewAnimation/app/src/main/java/com/example/viewanimation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.viewanimation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.*
@@ -11,11 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+
         binding = ActivityMainBinding.inflate(layoutInflater)
 
 
         setContentView(binding.root)
 
+
+        title = "MainActivity"
         binding.start.setOnClickListener {button->
             val myResAnimSet = AnimationUtils.loadAnimation(this, R.anim.my_anim)
 
@@ -44,6 +49,14 @@ class MainActivity : AppCompatActivity() {
 //            }
 //
 //            button.startAnimation(myAnimationSet)
+
+        }
+
+
+        binding.startActivity.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_right_in, R.anim.slide_left_out)
 
         }
 
