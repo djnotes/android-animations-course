a7a90d0e41b9e6bcfc2bf654a68f24b73cf29162
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2022-07-31 17:42:47 -0800

    Added AVD handling code

--- 2022/MyVectorGraphics/app/src/main/java/com/codewithmehdi/myvectorgraphics/MainActivity.kt
+++ 2022/MyVectorGraphics/app/src/main/java/com/codewithmehdi/myvectorgraphics/MainActivity.kt
@@ -3,6 +3,7 @@ package com.codewithmehdi.myvectorgraphics
 import android.graphics.drawable.AnimatedVectorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.view.forEach
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
@@ -25,6 +26,23 @@ class MainActivity : AppCompatActivity() {
                     (view.drawable as AnimatedVectorDrawable).start()
                 }
             }
+        }
+
+
+        binding.startAVDDynamic.setOnClickListener{
+            val avd = AnimatedVectorDrawableCompat.create(this, R.drawable.avd_progress_circle)
+            findViewById<ViewGroup>(android.R.id.content).addView(
+                ImageView(this).apply{
+                    setImageDrawable(avd)
+                }
+            )
+
+            avd?.start()
+        }
+
+        binding.startAVDBackground.setOnClickListener{button->
+            button.setBackgroundResource(R.drawable.avd_writing_tick)
+            (button.background as AnimatedVectorDrawable).start()
         }
 
 
