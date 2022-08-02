67899fe1be80c5f358c7726b3bf7aeeef84a546f
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2022-08-01 18:05:10 -0800

    Prepared for fling animation

--- 2022/MyPhysicalAnimationApp/app/src/main/java/com/codewithmehdi/myphysicalanimationapp/MainActivity.kt
+++ 2022/MyPhysicalAnimationApp/app/src/main/java/com/codewithmehdi/myphysicalanimationapp/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.codewithmehdi.myphysicalanimationapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.codewithmehdi.myphysicalanimationapp.databinding.*
+import androidx.appcompat.app.AppCompatActivity
+import com.codewithmehdi.myphysicalanimationapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
 
+
+
+
+        val myViews = listOf(
+            binding.sun,
+            binding.cloud1,
+            binding.cloud2,
+            binding.bird
+        )
+
+        //Initialize gesture listeners
+        myViews.forEach {image->
+
+        }
 
 
 
