449538ac10cd93634b9b73172ad8c2ac1adfb817
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2022-08-04 08:31:03 -0800

    Added navigation

--- 2022/PlaywithMotionLayout/app/src/main/java/com/codewithmehdi/playwithmotionlayout/ComplexMotionFragment.kt
+++ 2022/PlaywithMotionLayout/app/src/main/java/com/codewithmehdi/playwithmotionlayout/ComplexMotionFragment.kt
@@ -5,21 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.codewithmehdi.playwithmotionlayout.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-private lateinit var binding: FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [ComplexMotionFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class ComplexMotionFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,14 +34,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val navController = findNavController()
-        binding.makeComplexMotions.setOnClickListener {
-            navController.navigate(R.id.complex_motion)
-        }
-        return binding.root
-//        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_complex_motion, container, false)
     }
 
     companion object {
@@ -54,12 +44,12 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment ComplexMotionFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            ComplexMotionFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
