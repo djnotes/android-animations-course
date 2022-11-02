ed82931e6ffada6fa51f923b1dbe2536de6efb92
Mehdi Haghgoo <codewithmehdi@proton.me>
2022-11-02 15:37:59 +0330

    Recorded animatorset video

--- 2022/PropertyAnimation/app/src/main/java/com/codewithmehdi/mypropertyanimation/AnimatorsetFragment.kt
+++ 2022/PropertyAnimation/app/src/main/java/com/codewithmehdi/mypropertyanimation/AnimatorsetFragment.kt
@@ -2,6 +2,8 @@ package com.codewithmehdi.mypropertyanimation
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.graphics.Color
+import android.graphics.Rect
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -37,7 +39,33 @@ class AnimatorsetFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_animatorset, container, false)
+        return FragmentAnimatorsetBinding.inflate(layoutInflater).apply{
+            animateMultipleViews.setOnClickListener {
+                val  titleAnim = ObjectAnimator.ofFloat(title, "rotation", 0f, 360f)
+                val plusAnim = ObjectAnimator.ofFloat(plusImage, "scaleX", 1f, 2f)
+                val buttonAnim = ObjectAnimator.ofArgb(animateView, "backgroundColor", Color.BLUE, Color.RED)
+                AnimatorSet().apply{
+//                    playTogether(
+//                        titleAnim,
+//                        plusAnim,
+//                        buttonAnim
+//                    )
+//                    playSequentially(
+//                        buttonAnim,
+//                        plusAnim,
+//                        titleAnim
+//                    )
+
+                    play(titleAnim)
+                        .with(plusAnim)
+                        .after(buttonAnim)
+
+                    duration = 2000
+                    start()
+                }
+            }
+        }.root
+//        return inflater.inflate(R.layout.fragment_animatorset, container, false)
     }
 
     companion object {
