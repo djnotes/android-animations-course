dd5772bba16edb25f29748fd5254bfaadfa3950b
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2022-11-07 13:00:24 +0330

    Custom type animation - start

--- 2022/PropertyAnimation/app/src/main/java/com/codewithmehdi/mypropertyanimation/CustomTypeAnimationFragment.kt
+++ 2022/PropertyAnimation/app/src/main/java/com/codewithmehdi/mypropertyanimation/CustomTypeAnimationFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.codewithmehdi.mypropertyanimation.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [CustomTypeAnimationFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class CustomTypeAnimationFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,33 +34,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_home, container, false)
-
-        val navController = findNavController()
-
-        return FragmentHomeBinding.inflate(layoutInflater).apply{
-            valueAnimator.setOnClickListener {
-                navController.navigate(R.id.action_homeFragment_to_valueAnimatorFragment)
-            }
-
-            objectAnimator.setOnClickListener {
-                navController.navigate(R.id.action_homeFragment_to_objectAnimatorFragment)
-
-            }
-
-            viewPropertyAnimator.setOnClickListener {
-                navController.navigate(R.id.action_homeFragment_to_viewPropertyAnimatorFragment)
-            }
-
-            animatorSet.setOnClickListener{
-                navController.navigate(R.id.action_homeFragment_to_animatorsetFragment)
-            }
-
-            animateCustomType.setOnClickListener {
-                navController.navigate(R.id.action_homeFragment_to_customTypeAnimationFragment)
-            }
-
-        }.root
+        return inflater.inflate(R.layout.fragment_custom_type_animation, container, false)
     }
 
     companion object {
@@ -72,12 +44,12 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment CustomTypeAnimation.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            CustomTypeAnimationFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
